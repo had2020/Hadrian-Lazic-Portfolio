61f0412dc7d74d887a6732de895e0ce5788d7027
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2024-10-15 18:10:19 -0700

    Update main.rs

--- Backend/src/main.rs
+++ Backend/src/main.rs
@@ -1,13 +1,34 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::http::Status;
+use rocket::response::Redirect;
+
+/* String
 #[get("/")]
-fn index() -> String{
+fn index() -> String {
     String::from("Hello World!")
+}
+*/
+
+#[get("/")]
+fn index() -> Redirect {
+    let msg: Option<&str> = None;
+    Redirect::to(uri!(google_keep_desktop_api(
+        "windows-x86_64",
+        "v1.0.14",
+        msg
+    )))
+}
+
+#[get("/google-keep-desktop/<_platform>/<current_version>?<msg>")]
+fn google_keep_desktop_api(_platform: &str, current_version: &str, msg: Option<&str>) -> Status {
+    Status::NoContent
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![index])
+        .mount("/tauri-releases", routes![google_keep_desktop_api])
 }
