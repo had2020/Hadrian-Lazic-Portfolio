1b07277526b6024fcf41340dc4a22250743fe294
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2024-10-15 18:47:25 -0700

    basic api host test

--- Backend/src/main.rs
+++ Backend/src/main.rs
@@ -1,8 +1,10 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket::http::Status;
+use rocket::http::Status; // for returning no content / Status
 use rocket::response::Redirect;
+use rocket::http::uri::Origin;
+use rocket::serde::json::{json, Value};
 
 /* String
 #[get("/")]
@@ -11,24 +13,32 @@ fn index() -> String {
 }
 */
 
+const TAURI_RELEASES_PREFIX: Origin<'static> = uri!("/tauri-releases");
+
 #[get("/")]
 fn index() -> Redirect {
     let msg: Option<&str> = None;
-    Redirect::to(uri!(google_keep_desktop_api(
-        "windows-x86_64",
-        "v1.0.14",
-        msg
-    )))
+    Redirect::to(uri!(TAURI_RELEASES_PREFIX,google_keep_desktop_api("windows-x86_64", "v1.0.14", msg)))
 }
 
 #[get("/google-keep-desktop/<_platform>/<current_version>?<msg>")]
-fn google_keep_desktop_api(_platform: &str, current_version: &str, msg: Option<&str>) -> Status {
-    Status::NoContent
+fn google_keep_desktop_api(_platform: &str, current_version: &str, msg: Option<&str>) -> Result<Value, Status> { // can return-> Value or Status as well
+    // Status::NoContent
+    // error prone logic -> Option / Result
+    if let Some(msg) = msg { // if msg contatins something
+        println!("{msg}"); // needs to remain in format
+        format!("{msg}"); // same thing
+        return Err(Status::NoContent); // can be seen in network panel
+    }
+
+    Ok(json!({ // the Ok means correct value
+        "notes": "IT WORKS"
+    }))
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![index])
-        .mount("/tauri-releases", routes![google_keep_desktop_api])
+        .mount(TAURI_RELEASES_PREFIX, routes![google_keep_desktop_api])
 }
